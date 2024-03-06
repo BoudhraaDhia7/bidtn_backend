942b0b4717736f0d1f65a830f97172744a29c861
Boudhraa Dhiaedine <88516304+BoudhraaDhia7@users.noreply.github.com>
2024-03-06 22:23:09 +0100

    Update app.php, jwt.php, reset-password.blade.php, Role.php, and messages.php

--- resources/lang/fr/messages.php
+++ resources/lang/fr/messages.php
@@ -4,9 +4,15 @@ return [
     'user_authenticated' => 'Utilisateur authentifié avec succès',
     'user_registred' => 'Utilisateur enregistré avec succès',
     'user_logout' => 'Utilisateur déconnecté avec succès',
-    'user_not_found' => 'Utilisateur non trouvé',
-    'user_authenticated_failed' => 'Echec d\'authentification de l\'utilisateur',
+    'user_authenticated_failed' => 'Veuillez vérifier vos identifiants et réessayer',
     'user_registration_failed' => 'Echec d\'enregistrement de l\'utilisateur',
+    'user_detail' => 'Détails de l\'utilisateur récupérés avec succès',
+    'user_detail_failed' => 'Echec de récupération des détails de l\'utilisateur',
+    'user_updated' => 'Détails de l\'utilisateur mis à jour avec succès',
+    'user_not_found' => 'L\'email fourni n\'est associé à aucun compte',
+    'user_deleted' => 'Utilisateur supprimé avec succès',
+    'user_update_failed' => 'Echec de mise à jour des détails de l\'utilisateur',
+    'user_password_reset' => 'Votre mot de passe a été réinitialisé avec succès',
 
 
     //Token
@@ -21,6 +27,17 @@ return [
 
     //media
     'media_creation_failed' => 'Echec de création de média',
+    'media_deleting__failed' => 'Echec de suppression de média',
+
+    //email
+    'reset_password_subject' => 'Réinitialisez votre mot de passe',
+    'set_password_subject' => 'Définissez votre mot de passe',
+    'email_sended' => 'Email envoyé avec succès',
+    
+    //Link
+    'invalid_link' => 'lien invalide ou expireé',
+
     //500 errors
     'internal_server_error' => 'Erreur interne du serveur',
+    'general_error' => 'Quelque chose s\'est mal passé, veuillez réessayer plus tard',
 ];
---
+++ resources/views/emails/users/reset-password.blade.php
@@ -0,0 +1,16 @@
+@component('mail::message')
+    # Set Your Password
+
+    You are receiving this email because an account was created for you. Please set your password by clicking the button below.
+
+
+
+    If you did not request an account, no further action is required.
+
+    Thanks,<br>
+
+    @component('mail::button', ['url' => $resetUrl])
+        Set Password
+    @endcomponent
+
+@endcomponent
